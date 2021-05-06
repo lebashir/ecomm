c45ad7c00022e085252c1a7b769acea622de9523
lebashir <bashir.abihabib@gmail.com>
2021-05-06 15:56:53 -0500

    added simple form and parsing functionality

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hi there!');
+    res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email">
+            <input name="password" placeholder="password">
+            <input name="passwordConfirmation" placeholder="password confirmation">
+            <button>Sign Up</button>
+        </form>
+    </div>`);
+});
+
+app.post('/', bodyParser.urlencoded({extended: true}), (req, res) => {
+    console.log(req.body)
+    //const {email, password, passwordConfirmation } = await req.body;
+    res.send('Account Created!')
 });
 
 app.listen(3000, () => {

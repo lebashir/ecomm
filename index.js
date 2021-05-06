90150a15fbe870f81290bb5f4c37c77c795808cc
lebashir <bashir.abihabib@gmail.com>
2021-05-06 18:15:58 -0500

    added basic signup validation logic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const usersRepo = require('./repositories/users');
 
 const app = express();
 
@@ -17,9 +18,16 @@ app.get('/', (req, res) => {
     </div>`);
 });
 
-app.post('/',  (req, res) => {
-    console.log(req.body)
-    //const {email, password, passwordConfirmation } = await req.body;
+app.post('/',  async (req, res) => {
+    const {email, password, passwordConfirmation } = req.body;
+
+    const existingUser = await usersRepo.getOneBy({email})
+    if (existingUser) {
+        return res.send('Email in use');
+    }
+    if (password !== passwordConfirmation) {
+        return res.send('Passwords must match')
+    }
     res.send('Account Created!')
 });
 

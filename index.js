d344e865fbf776489b7b6dfaf8b90dcfe7db099c
lebashir <bashir.abihabib@gmail.com>
2021-05-06 15:59:10 -0500

    applied bodyParser globally

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.send(`
     <div>
@@ -15,7 +17,7 @@ app.get('/', (req, res) => {
     </div>`);
 });
 
-app.post('/', bodyParser.urlencoded({extended: true}), (req, res) => {
+app.post('/',  (req, res) => {
     console.log(req.body)
     //const {email, password, passwordConfirmation } = await req.body;
     res.send('Account Created!')

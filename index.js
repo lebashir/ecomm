62dd6bf48be839d0ee32a15035dda7eb9387e21b
lebashir <bashir.abihabib@gmail.com>
2021-05-06 20:43:52 -0500

    added sign in and sign out

--- index.js
+++ index.js
@@ -12,20 +12,21 @@ app.use(cookieSession({
     keys: ['vfdopikjyh0392390wpe48w48y']
 }));
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
     res.send(`
     <div>
-        Your id is: ${req.session.userId}
+        Your id is:${req.session.userId}
         <form method="POST">
             <input name="email" placeholder="email">
             <input name="password" placeholder="password">
             <input name="passwordConfirmation" placeholder="password confirmation">
             <button>Sign Up</button>
         </form>
-    </div>`);
+    </div>
+    `);
 });
 
-app.post('/',  async (req, res) => {
+app.post('/signup',  async (req, res) => {
     const {email, password, passwordConfirmation } = req.body;
 
     const existingUser = await usersRepo.getOneBy({email})
@@ -44,6 +45,39 @@ app.post('/',  async (req, res) => {
     
     res.send('Account Created!')
 });
+
+app.get('/signout', (req, res) => {
+    req.session = null;
+    res.send('You are logged out')
+    res.redirect('/signin');
+
+})
+
+app.get('/signin', (req, res) => {
+    res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email">
+            <input name="password" placeholder="password">
+            <button>Sign In</button>
+        </form>
+    </div>
+    `
+    )
+})
+
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await usersRepo.getOneBy({ email });
+    if (!user) {
+        return res.send('Email not found');
+    }
+    if (user.password !== password) {
+        return res.send('Invalid Credentials');
+    }
+    req.session.userId = user.Id;
+    res.send('You are signed in!!!')
+})
 
 app.listen(3000, () => {
     console.log('listening')

e8e93aacda868a5b5128727fdd59faaae43d4a1b
lebashir <bashir.abihabib@gmail.com>
2021-05-07 14:09:41 -0500

    added multer and image upload functionality

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { validationResult } = require('express-validator');
+const multer = require('multer');
+const products = require('../../repositories/products');
 
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators')
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage()});
 
 router.get('/admin/products', (req, res) => {
 
@@ -16,9 +19,21 @@ router.get('/admin/products/new', (req, res) => {
 
 });
 
-router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
+router.post('/admin/products/new',
+    upload.single('image'), 
+    [requireTitle, requirePrice],
+    async (req, res) => {
+
     const errors = validationResult(req);
-    console.log(errors);
+    if (!errors.isEmpty()) {
+        return res.send(productsNewTemplate({errors}));
+    }
+    
+    const image = req.file.buffer.toString('base64');
+    const { title, price } = req.body;
+    const product = await productsRepo.create({title, price, image})
+
+
     res.send('submitted')
 })
 

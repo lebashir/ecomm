5b7a12bb82ecb6c5f7bc2043d41cb5bf63a8d15f
lebashir <bashir.abihabib@gmail.com>
2021-05-08 11:05:39 -0500

    fixed some module issues

--- index.js
+++ index.js
@@ -4,19 +4,18 @@ const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
 const productsRouter = require('./routes/admin/products');
 
-
 const app = express();
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(cookieSession({
-    keys: ['vfdopikjyh0392390wpe48w48y']
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ['lkasld235j']
+  })
+);
 app.use(authRouter);
 app.use(productsRouter);
 
-
 app.listen(3000, () => {
-    console.log('listening')
+  console.log('Listening');
 });
